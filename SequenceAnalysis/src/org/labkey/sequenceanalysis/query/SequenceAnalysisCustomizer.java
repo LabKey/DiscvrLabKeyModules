32d05342b81027c2a7e565d95330ed9c400e7a14
labkey-jeckels <jeckels@labkey.com>
2026-08-23 09:52:24 -0700

    Remove SQL Server upgrade scripts and code

--- SequenceAnalysis/src/org/labkey/sequenceanalysis/query/SequenceAnalysisCustomizer.java
+++ SequenceAnalysis/src/org/labkey/sequenceanalysis/query/SequenceAnalysisCustomizer.java
@@ -53,30 +53,9 @@ public class SequenceAnalysisCustomizer implements TableCustomizer
         {
             LDKService.get().getDefaultTableCustomizer().customize(ti);
 
-            if (tableInfo.getName().equalsIgnoreCase(SequenceAnalysisSchema.TABLE_REF_NT_SEQUENCES))
-            {
-                //behaves slowly on postgres
-                if (tableInfo.getSqlDialect().isSqlServer())
-                {
-                    LDKService.get().applyNaturalSort(ti, "name");
-                }
-            }
-            else if (tableInfo.getName().equalsIgnoreCase(SequenceAnalysisSchema.TABLE_INSTRUMENT_RUNS))
-            {
-                if (tableInfo.getSqlDialect().isSqlServer())
-                {
-                    LDKService.get().applyNaturalSort(ti, "name");
-                }
-            }
-            else if (tableInfo.getName().equalsIgnoreCase(SequenceAnalysisSchema.TABLE_OUTPUTFILES))
+            if (tableInfo.getName().equalsIgnoreCase(SequenceAnalysisSchema.TABLE_OUTPUTFILES))
             {
                 LaboratoryService.get().getLaboratoryTableCustomizer().customize(tableInfo);
-
-                //behaves slowly on postgres
-                if (tableInfo.getSqlDialect().isSqlServer())
-                {
-                    LDKService.get().applyNaturalSort(ti, "name");
-                }
 
                 addFileSetCol(ti);
             }
